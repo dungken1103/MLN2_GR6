3eca9684ab51e6cbf30bdb113111caf39007980b
Dung <dungtdhe186461@fpt.edu.vn>
2026-03-17 11:08:26 +0700

    .

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import './index.css'
+
+function App() {
+  return (
+    <h1 className="text-4xl font-bold text-blue-500">
+      Hello Tailwind 3.4 🚀
+    </h1>
+  )
+}
+
+export default App
